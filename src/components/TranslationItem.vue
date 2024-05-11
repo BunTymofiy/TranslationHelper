063e22afd2699cfd0568bfd8794c5ad6f94bb1a6
Tymofiy Bun <tima.boon@gmail.com>
2024-05-11 17:48:10 -0400

    add component :)

---
+++ src/components/TranslationItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <td v-for="translation in listOfAllTranslations" :key="translation[listKey]">
+    <TranslationItem
+      v-if="typeof translation[listKey] == 'object'"
+      :listOfAllTranslations="listOfAllTranslations"
+      :listKey="listKey"
+    />
+    <div v-else>
+      <td>{{ translation[listKey] }}</td>
+    </div>
+  </td>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  listOfAllTranslations: Record<string, any>[]
+  listKey: string
+}
+defineProps<Props>()
+</script>
+
+<style scoped></style>
