fe46d9cf150443e74b2821bc7eb447b82be41e44
Tymofiy Bun <tima.boon@gmail.com>
2024-05-11 17:43:00 -0400

    WIP 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import localeMessagesEn from '@/assets/files_to_translate/en.js'
-import localeMessagesFr from '@/assets/files_to_translate/fr.js'
-import localeMessagesAr from '@/assets/files_to_translate/ar.js'
-import localeMessagesHe from '@/assets/files_to_translate/he.js'
-import localeMessagesSv from '@/assets/files_to_translate/sv.js'
-import localeMessagesDe from '@/assets/files_to_translate/de.js'
-
-let en = ref(localeMessagesEn)
-let fr = ref(localeMessagesFr)
-let ar = ref(localeMessagesAr)
-let he = ref(localeMessagesHe)
-let sv = ref(localeMessagesSv)
-let de = ref(localeMessagesDe)
-</script>
-
 <template>
   <VTable>
     <thead>
@@ -30,36 +13,51 @@ let de = ref(localeMessagesDe)
     </thead>
     <tbody>
       <tr v-for="key in Object.keys(en)" :key="key">
-        <div v-if="typeof key === 'object'">
-          <VTable v-for="subKey in Object.keys(key)" :key="subKey">
-            <tbody>
-              <tr v-for="subKey in Object.keys(key)" :key="subKey">
+        <td>{{ key }}</td>
+        <TranslationItem :list-of-all-translations="listOfAllTranslations" :list-key="key" />
+        <!-- <td v-for="translation in listOfAllTranslations" :key="translation[key]">
+          <div v-if="typeof(translation[key]) == 'object'">
+            <div v-for="subKey in Object.keys(translation)" :key="subKey">
+              <div>{{ subKey }}</div>
+              <div>{{ translation[subKey] }}</div>
+            </div>
+            <VTable>
+              <tr v-for="subKey in Object.keys(translation)" :key="subKey">
                 <td>{{ subKey }}</td>
-                <td>{{ en[key][subKey] }}</td>
-                <td>{{ fr[key][subKey] }}</td>
-                <td>{{ ar[key][subKey] }}</td>
-                <td>{{ he[key][subKey] }}</td>
-                <td>{{ sv[key][subKey] }}</td>
-                <td>{{ de[key][subKey] }}</td>
+                <td>{{ translation[subKey] }}</td>
               </tr>
-            </tbody>
-          </VTable>
-        </div>
-        <div v-else>
-          <td>{{ key }}</td>
-          <td>{{ en[key] }}</td>
-          <td>{{ fr[key] }}</td>
-          <td>{{ ar[key] }}</td>
-          <td>{{ he[key] }}</td>
-          <td>{{ sv[key] }}</td>
-          <td>{{ de[key] }}</td>
-        </div>
+            </VTable>
+          </div>
+          <div v-else>
+            {{ translation[key] }}
+          </div>
+        </td> -->
       </tr>
     </tbody>
   </VTable>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+import localeMessagesEn from '@/assets/files_to_translate/en.js'
+import localeMessagesFr from '@/assets/files_to_translate/fr.js'
+import localeMessagesAr from '@/assets/files_to_translate/ar.js'
+import localeMessagesHe from '@/assets/files_to_translate/he.js'
+import localeMessagesSv from '@/assets/files_to_translate/sv.js'
+import localeMessagesDe from '@/assets/files_to_translate/de.js'
+import TranslationItem from '@/components/TranslationItem.vue'
+
+const en = <Record<string,any>>(localeMessagesEn)
+const fr = <Record<string,any>>(localeMessagesFr)
+const ar = <Record<string,any>>(localeMessagesAr)
+const he = <Record<string,any>>(localeMessagesHe)
+const sv = <Record<string,any>>(localeMessagesSv)
+const de = <Record<string,any>>(localeMessagesDe)
+const listOfAllTranslations = ref<Record<string,any>[]>([en, fr, ar, he, sv, de])
+console.log(listOfAllTranslations.value);
+</script>
+
+
+
 <style scoped>
-.v-table {
-}
 </style>
